5b75418a08f10bedc3005760949343d524d3b812
Peter <pkabai@csumb.edu>
2018-10-11 18:21:04 -0700

    Adds new pages

--- zoo-animals/src/app/app.module.ts
+++ zoo-animals/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReviewComponent } from './review/review.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailsComponent } from './details/details.component';
+import { LoginComponent } from './login/login.component';
+import { ItemsComponent } from './items/items.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReviewComponent,
+    CartComponent,
+    DetailsComponent,
+    LoginComponent,
+    ItemsComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
