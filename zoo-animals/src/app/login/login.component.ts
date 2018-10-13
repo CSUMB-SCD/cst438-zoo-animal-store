7915bee684d9dad197acb454d0888962af99a864
Peter <pkabai@csumb.edu>
2018-10-12 20:27:14 -0700

    Adds sign in functionality

--- zoo-animals/src/app/login/login.component.ts
+++ zoo-animals/src/app/login/login.component.ts
@@ -9,13 +9,35 @@ import { DataService } from '../data.service';
 
 export class LoginComponent implements OnInit {
   users$: Object;
-  
+
   constructor(private data: DataService) {}
+
+  onSubmit(username, password) {
+    let i = 0
+    let size = 0
+    let success = false;
+    for (let user in this.users$) {
+      size++;
+    }
+    while (i < size) {
+      if (this.users$[i].username == username && this.users$[i].password == password) {
+        success = true;
+      }
+      i++;
+    }
+    if (success) {
+      console.log("Logged In!");
+      document.getElementById("errorText").style.display = "none";
+    }
+    else {
+      console.log("Login Failed");
+      document.getElementById("errorText").style.display = "block";
+    }
+  }
 
   ngOnInit() {
     this.data.getUsers().subscribe(
       data => this.users$ = data 
     );
   }
-
 }
