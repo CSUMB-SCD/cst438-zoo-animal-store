40a0e7d63ef437fb98e6e7f63401f89cdca4ce16
Victoria <vzamora@csumb.edu>
2018-11-13 16:15:31 -0800

    Deploying to Heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/lab'));
+app.use(express.static(__dirname + '/dist/zoo-animals'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/lab/index.html'));
+res.sendFile(path.join(__dirname+'/dist/zoo-animals/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
