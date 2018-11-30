dce03b55dbf7791f120d179b362556341c8cba86
memej <adiaz@csumb.edu>
2018-11-29 18:36:30 -0800

    added button to homepage for sign in

--- src/main/java/spring/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package spring;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-@Repository
-interface UsersRepository extends MongoRepository<Users, String> {
-    Users findByusername(String username);
-}
